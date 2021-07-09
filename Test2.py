9c33b769ce774297b27aa523e71f55cfacc0f39a
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-07-09 13:35:45 +0430

    00-04-18-second

--- Test2.py
+++ Test2.py
@@ -11,3 +11,25 @@ c = names.count('amirhossein')
 print('count of amirhossein is => ',c)
 print(names.index('amirhossein'))
 print(names.index('amirhossein',2,5))
+w = [12, 22, 33, 44, 55]
+# del w[:]
+# del w
+w.clear()
+print(w)
+names.reverse()
+print(names)
+new_names = names[::-1]
+print(new_names)
+
+a = [1, 2, 3]
+b = a
+b[0] = 12
+print(b)
+print(a)
+min_a = min(a)
+max_a = max(a)
+print(min_a,max_a)
+r = list(range(0,5))
+print(r)
+r = range(5)[-1]
+print(r)
